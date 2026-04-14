3563ee06c967c205c87540e2b63ff47e8d26717c
Chi Nguyen <buichinguyen1105@gmail.com>
2026-04-14 23:35:12 +0300

    RPC structs and reformat using naming convention

--- raft.c
+++ raft.c
@@ -2,36 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-RaftNode *init_raft_node(int id, int numPeers) {
-    RaftNode *R = calloc(1, sizeof(RaftNode));
-    R->id = id;
-    R->numPeers = numPeers;
-    R->role = FOLLOWER;
-    R->votedFor = -1;
-    R->log = calloc(INITIAL_LOG_CAPACITY, sizeof(LogEntry));
-    R->logCapacity = INITIAL_LOG_CAPACITY;
-    return R;
+raft_node_t *init_raft_node(int id, int num_peers) {
+    raft_node_t *node = calloc(1, sizeof(raft_node_t));
+    node->id = id;
+    node->num_peers = num_peers;
+    node->role = FOLLOWER;
+    node->voted_for = -1;
+    node->log = calloc(INITIAL_LOG_CAPACITY, sizeof(log_entry_t));
+    node->log_capacity = INITIAL_LOG_CAPACITY;
+    return node;
 }
 
-void become_leader(RaftNode *R) {
-    R->role = LEADER; 
-    int numPeers = R->numPeers;
-    int logLength = R->logLength;
-    R->nextIndex = malloc(numPeers * sizeof(int));
-    for (int i = 0; i < numPeers; i++) {
-        R->nextIndex[i] = logLength;
+void become_leader(raft_node_t *node) {
+    node->role = LEADER; 
+    int num_peers = node->num_peers;
+    int log_length = node->log_length;
+    node->next_index = malloc(num_peers * sizeof(int));
+    for (int i = 0; i < num_peers; i++) {
+        node->next_index[i] = log_length;
     }
-    R->matchIndex = calloc(numPeers, sizeof(int));
+    node->match_index = calloc(num_peers, sizeof(int));
 }
 
-void step_down(RaftNode *R, int newTerm) {
-    R->currentTerm = newTerm;
-    R->votedFor = -1;
-    if (R->role == LEADER) {
-        free(R->nextIndex);
-        free(R->matchIndex);
-        R->nextIndex = NULL;
-        R->matchIndex = NULL;
+void step_down(raft_node_t *node, int new_term) {
+    node->current_term = new_term;
+    node->voted_for = -1;
+    if (node->role == LEADER) {
+        free(node->next_index);
+        free(node->match_index);
+        node->next_index = NULL;
+        node->match_index = NULL;
     }
-    R->role = FOLLOWER;
+    node->role = FOLLOWER;
 }
--- raft.h
+++ raft.h
@@ -1,40 +1,67 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
 #ifndef RAFT_H
 #define RAFT_H
 
 #define INITIAL_LOG_CAPACITY 8
 
-typedef enum { FOLLOWER, CANDIDATE, LEADER } Role;
+typedef enum { FOLLOWER, CANDIDATE, LEADER } role_t;
 
 typedef struct {
   int term;
   char command[256];
-} LogEntry;
+} log_entry_t;
 
 typedef struct {
   // Voting
-  int currentTerm;
-  int votedFor;
+  int current_term;
+  int voted_for;
 
   // Log
-  LogEntry *log;
-  int logLength;
-  int logCapacity;
+  log_entry_t *log;
+  int log_length;
+  int log_capacity;
   
   // Volatile 
-  int commitIndex;
-  int lastApplied;
+  int commit_index;
+  int last_applied;
 
   // Leader-only state
-  int *nextIndex;
-  int *matchIndex;
+  int *next_index;
+  int *match_index;
 
-  Role role;
+  role_t role;
   int id;
-  int numPeers;
-} RaftNode;
+  int num_peers;
+} raft_node_t;
 
-RaftNode *init_raft_node(int id, int numPeers);
-void become_leader(RaftNode *R);
-void step_down(RaftNode *R, int newTerm);
+raft_node_t *init_raft_node(int id, int num_peers);
+void become_leader(raft_node_t *node);
+void step_down(raft_node_t *node, int new_term);
+
+// RPC structs
+typedef struct {
+  int term;
+  bool vote_granted;
+} request_vote_reply_t;
+
+typedef struct {
+  int candidate_term;
+  int candidate_id;
+  int term_last_entry;
+  int index_last_entry;
+} request_vote_args_t;
+
+typedef struct {
+  int leader_term;
+  int leader_id;
+  int prev_log_term;
+  int prev_log_index;
+  log_entry_t *new_entries;
+  int entries_count;
+  int commit_index;
+} append_entries_args_t;
 
 #endif
